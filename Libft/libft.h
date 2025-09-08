43a7be7751fe2455dfecd8bb4a48b3db48ff6afd
InZonyx <arnomoureaux@MacBook-Pro-de-Arno.local>
2025-09-08 21:34:57 +0200

    isascii

---
+++ Libft/libft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amoureau <amoureau@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/08 17:51:53 by amoureau          #+#    #+#             */
+/*   Updated: 2025/09/08 17:57:23 by amoureau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef libft_h
+# define libft_h
+
+# include <unistd.h>
+# include <stdlib.h>
+
+int ft_isalpha(int c);
+int ft_isdigit(int c);
+int ft_isalnum(int c);
+
+#endif
